7934b34cb57b33a4d485d15ac22a0abfaf1fb6cd
EnasQtait <enasmtq14@gmail.com>
2026-02-23 23:48:19 +0200

    Initial commit: APB FIFO UVM Verification

--- tb/my_sequencer.sv
+++
@@ -1,6 +0,0 @@
-class my_sequencer extends uvm_sequencer #(transaction);
-  `uvm_component_utils(my_sequencer)
-  function new(string name, uvm_component parent);
-    super.new(name,parent);
-  endfunction
-endclass
